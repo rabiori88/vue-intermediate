be9c87206ea256808979b015dabb9cd54cb5ba31
Rabiori <ctangtangbbang@gmail.com>
2023-01-15 23:44:54 +0900

    Todo list 기본 세팅 완료

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="str" v-on:keyup.enter="addTodo">
+    <!-- <button v-on:click='addTodo'> add</button> -->
+    <span class="addContainer" v-on:click="addTodo">
+      
+      <i class="fa-regular fa-plus fa-flip addBtn"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data: function() {
+      return  {
+        str: ''
+      }
+    },
+    methods: {
+      addTodo: function() {
+        console.log(this.str);     
+
+        localStorage.setItem(this.str, this.str);
+        // localStorage.getItem(this.str);
+        this.clearInput();
+      },
+
+      clearInput: function() {
+        this.str = '';
+      }
+    }
+
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+
+.inputBox {
+  background-color: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+
+}
+
+.inputBox input {
+  border-style : none;
+  font-size : 0.9rem;
+}
+
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+
+</style>
